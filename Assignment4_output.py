1f3fd603a68c20543a045108799b78c7f896ab22
shlshambhavi <104293661+shlshambhavi@users.noreply.github.com>
2022-06-09 20:08:49 +0530

    Add files via upload

---
+++ Assignment4_output.py
@@ -0,0 +1,39 @@
+Q1
+Enter mark:78
+Grade: B
+Enter mark:56
+Grade: C
+Enter mark:45
+Grade: D
+Enter mark:78
+Grade: B
+Enter mark:56
+Grade: C
+Enter mark:78
+Grade: B
+Q2
+Enter year:2000
+Leap year
+Q3
+Question 1:3x0=0
+Right!
+Question 2:6x8=56
+Wrong. The correct answer is 48
+Question 3:7x6=42
+Right!
+Question 4:6x7=45
+Wrong. The correct answer is 42
+Question 5:9x0=0
+Right!
+Question 6:0x7=0
+Right!
+Question 7:6x2=12
+Right!
+Question 8:0x2=4
+Wrong. The correct answer is 0
+Question 9:7x6=72
+Wrong. The correct answer is 42
+Question 10:9x5=45
+Right!
+Q4
+Answer: 177
