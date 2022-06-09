1ad94916cf5f1c180661c159b7997bbd9cdd54cb
shlshambhavi <104293661+shlshambhavi@users.noreply.github.com>
2022-06-09 19:49:09 +0530

    Add files via upload

---
+++ Assignment3_output.py
@@ -0,0 +1,48 @@
+Q1
+Enter a number4
+Binary equivalent of 4 is 100
+Q2
+Enter expression:2+8-9
+2+8-9 = 1
+Q3
+a)35
+b)enter the value of n:10
+45.0
+c)enter the value of radius:7
+616.0
+e)the value of y1 is:6
+the value of y2 is:9
+the value of x1 is:5
+the value of x2 is:8
+1.0
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:5
+Enter number of hydrogen atoms in molecule:7
+Enter number of oxygen atoms in molecule:3
+The molecular weight of the compound is 115.10728
